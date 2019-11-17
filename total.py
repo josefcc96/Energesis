317d8cf0de54c8fc5eafed5fb17f805495786232
Jose Campo <jose.francisco.campo.campo@gmail.com>
2019-11-17 04:31:42 -0500

    Update total.py

--- total.py
+++ total.py
@@ -189,7 +189,7 @@ def segundx(numero, fecha_sms, id_sms):
 				print(str(posi)+"--"+consumo+"--"+t1+"--"+h1+"--"+t2+"--"+h2+"--"+t3+"--"+h3+"--"+t4+"--"+h4+"--"+fecha+"--"+hora+"--"+crc)
 				response = requests.post('https://graphql.cclimamagdalena.com/api/v1/houses/simple', data = {'numCasa':id_Guamal[posi], 'consumption': consumo, 't1': t1,'h1': h1, 't2': t2, 'h2': h2, 't3': t3,'h3': h3, 'date': fecha,'hour':hora})
 				json_response = response.json()
-				json_response['data']
+			    # json_response['data']
 				dato+=1
 
 		elif "\r\n" in segunda: 
@@ -413,7 +413,7 @@ while True:
 	except KeyboardInterrupt:
 		GPIO.cleanup()
 		infinito = False
-		control=False
+		control = False
 		serie.close()
 		#cursor.close()
 		cursor_rpi.close()
