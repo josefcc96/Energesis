e249ba3a2f637c2564745df349cc4e8b32246487
Jose Campo <jose.francisco.campo.campo@gmail.com>
2019-11-17 13:21:51 -0500

    Update total.py

--- total.py
+++ total.py
@@ -188,7 +188,19 @@ def segundx(numero, fecha_sms, id_sms):
 				posi=num_Guamal.index(numero)
 				consumo,t1,h1,t2,h2,t3,h3,t4,h4,fecha,hora,crc=segunda.split(',')
 				print(str(posi)+"--"+consumo+"--"+t1+"--"+h1+"--"+t2+"--"+h2+"--"+t3+"--"+h3+"--"+t4+"--"+h4+"--"+fecha+"--"+hora+"--"+crc)
-				response = requests.post('https://graphql.cclimamagdalena.com/api/v1/houses/simple', data = {'numCasa':id_Guamal[posi], 'consumption': consumo, 't1': t1,'h1': h1, 't2': t2, 'h2': h2, 't3': t3,'h3': h3, 'date': fecha,'hour':hora})
+				datos={
+					"numCasa":id_Guamal[posi],
+					"consumption": consumo, 
+					"t1": t1,
+					"h1": h1, 
+					"t2": t2, 
+					"h2": h2, 
+					"t3": t3,
+					"h3": h3, 
+					"date": fecha,
+					"hour":hora
+				}
+				response = requests.post('https://graphql.cclimamagdalena.com/api/v1/houses/simple', data =JSON.stringify(datos))
 				#print(response)
 				json_response = response.json()
 				#json_response['data']
