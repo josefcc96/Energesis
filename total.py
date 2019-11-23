3836766e34e131f22493a22b015b1b69940bb2c7
Jose Campo <jose.francisco.campo.campo@gmail.com>
2019-11-23 01:00:05 -0500

    Update total.py

--- total.py
+++ total.py
@@ -259,15 +259,15 @@ def consulta_bdd(fecha_menor, fecha_mayor):
 
 
 def hum(hume):
-	if hume > 99.0:
+	if float(hume) > 99.0:
 		return 99.0
 	else:
 		return hume
 
-	if hume == 0.0:
+	if float(hume) == 0.0:
 		return 99.0
 	else:
-		return hume
+		return float(hume)
 
 
 	
